3c1b1b052fedd4d1b79ce167038b226b99556361
Guest <guest-a02las@h26817.sgb.skir.kr>
2021-01-07 23:10:39 +0900

    complete

--- main.js
+++ main.js
@@ -1,41 +1,22 @@
-/** 시간 */
-const hourElement = document.querySelector('.hour');
+const hour = document.getElementById('hour');
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
 
-/** 분 */
-const minuteElement = document.querySelector('.minute');
+onClock();
 
-/** 초 */
-const secondElement = document.querySelector('.second');
+function onClock(){
+    const time = new Date();
+    const h = time.getHours();
+    const m = time.getMinutes();
+    const s = time.getSeconds();
 
-update();
+    hour.innerHTML = makeLen(h);
+    min.innerHTML = makeLen(m);
+    sec.innerHTML = makeLen(s);
 
-/**
- * 현재 시간 표시 처리
- */
-function update() {
-  const currentTime = new Date();
-
-  // 시간 표시
-  const hour = currentTime.getHours();
-  hourElement.innerText = addZeroPadding(hour);
-
-  // 분 표시
-  const minute = currentTime.getMinutes();
-  minuteElement.innerText = addZeroPadding(minute);
-
-  // 초 표시
-  const second = currentTime.getSeconds();
-  secondElement.innerText = addZeroPadding(second);
-
-  // 프레임에서 update( ) 재실행
-  requestAnimationFrame(update);
+    requestAnimationFrame(onClock);
 }
 
-/**
-* 2자리 형식이 되도록 앞부분에 0을 추가하는 함수
- * @param num
- * @returns {string}
- */
-function addZeroPadding(num) {
-  return String(num).padStart(2, '0');
+function makeLen(num){
+    return String(num).padStart(2,'0');
 }
